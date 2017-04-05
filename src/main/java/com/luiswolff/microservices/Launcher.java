ee03e5adefb288c50f460e15a994efb1bcc872fd
Luis-Manuel Wolff Heredia <luis-manuel.wolff@outlook.com>
2017-04-05 11:29:22 +0200

    Added licence header to source files

--- src/main/java/com/luiswolff/microservices/Launcher.java
+++ src/main/java/com/luiswolff/microservices/Launcher.java
@@ -1,3 +1,23 @@
+/*
+ * Java-EE7-Microservice
+ * Copyright (C) 2017  Luis-Manuel Wolff Heredia
+ *
+ * This file is part of Java-EE7-Microservice.
+ *
+ * Java-EE7-Microservice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java-EE7-Microservice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Java-EE7-Microservice. It not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.luiswolff.microservices;
 
 import java.io.File;
@@ -16,7 +36,7 @@ import java.util.logging.Logger;
  * </ul>
  * <p>ATTENTION: This application is designed to deploy only one artifact.</p>
  *
- * @author Luis-Manuel Wolff Heredia <a href='mailto:l.wolff@bioeng.de'>mailto:l.wolff@bioeng.de</a>
+ * @author Luis-Manuel Wolff Heredia
  */
 public class Launcher {
 
